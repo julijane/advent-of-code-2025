6f2be99cf95fac51011cb8b255136b785b255563
Juliane Holzt <juliane@holzt.de>
2025-12-09 23:35:53 +0100

    refactor: day09

--- day09/day09.go
+++ day09/day09.go
@@ -4,67 +4,70 @@ import (
 	"github.com/julijane/advent-of-code-2025/aoc"
 )
 
-type edge struct {
+type twoCoord struct {
 	a, b aoc.Coordinate
 }
 
-func calc(input *aoc.Input, _, _ bool, params ...any) (any, any) {
-	nodes := aoc.Coordinates{}
-	edges := []edge{}
-
-	for i, line := range input.PlainLines() {
-		val := aoc.ExtractNumbers(line)
-		coord := aoc.Coordinate{X: val[0], Y: val[1]}
-
-		nodes = append(nodes, coord)
-		if i > 0 {
-			edges = append(edges, edge{a: nodes[i-1], b: coord})
-		}
+func convertCoords(a, b aoc.Coordinate) twoCoord {
+	return twoCoord{
+		a: aoc.Coordinate{
+			X: min(a.X, b.X),
+			Y: min(a.Y, b.Y),
+		},
+		b: aoc.Coordinate{
+			X: max(a.X, b.X),
+			Y: max(a.Y, b.Y),
+		},
 	}
-	edges = append(edges, edge{a: nodes[len(nodes)-1], b: nodes[0]})
+}
 
+func edgeOverlapsRect(edge, rect twoCoord) bool {
+	if edge.a.X == edge.b.X {
+		if edge.a.X <= rect.a.X || edge.a.X >= rect.b.X {
+			return false
+		}
+
+		return (edge.a.Y <= rect.a.Y && edge.b.Y > rect.a.Y) ||
+			(edge.a.Y < rect.b.Y && edge.b.Y >= rect.b.Y)
+	} else {
+		if edge.a.Y <= rect.a.Y || edge.a.Y >= rect.b.Y {
+			return false
+		}
+
+		return (edge.a.X <= rect.a.X && edge.b.X > rect.a.X) ||
+			(edge.a.X < rect.b.X && edge.b.X >= rect.b.X)
+	}
+}
+
+func calc(input *aoc.Input, _, _ bool, params ...any) (any, any) {
 	part1 := 0
 	part2 := 0
 
-	for i, a := range nodes {
-	rectloop:
-		for _, b := range nodes[i+1:] {
-			nodeXMin := min(a.X, b.X)
-			nodeXMax := max(a.X, b.X)
-			nodeYMin := min(a.Y, b.Y)
-			nodeYMax := max(a.Y, b.Y)
+	corners := aoc.Coordinates{}
+	for _, line := range input.PlainLines() {
+		val := aoc.ExtractNumbers(line)
+		corners = append(corners, aoc.Coordinate{X: val[0], Y: val[1]})
+	}
 
-			area := (nodeXMax - nodeXMin + 1) * (nodeYMax - nodeYMin + 1)
+	edges := []twoCoord{}
+	for i, cornerA := range corners {
+		cornerB := corners[(i+1)%len(corners)]
+		edges = append(edges, convertCoords(cornerA, cornerB))
+	}
+
+	for i, rectCornerA := range corners[1:] {
+	rectloop:
+		for _, rectCornerB := range corners[i+1:] {
+			rect := convertCoords(rectCornerA, rectCornerB)
+
+			area := (rect.b.X - rect.a.X + 1) * (rect.b.Y - rect.a.Y + 1)
 			if area > part1 {
 				part1 = area
 			}
 
-		lineloop:
 			for _, edge := range edges {
-				if edge.a.X == edge.b.X {
-					if edge.a.X <= nodeXMin || edge.a.X >= nodeXMax {
-						continue lineloop
-					}
-
-					edgeYMin := min(edge.a.Y, edge.b.Y)
-					edgeYMax := max(edge.a.Y, edge.b.Y)
-
-					if (edgeYMin <= nodeYMin && edgeYMax > nodeYMin) ||
-						(edgeYMin < nodeYMax && edgeYMax >= nodeYMax) {
-						continue rectloop
-					}
-				} else {
-					if edge.a.Y <= nodeYMin || edge.a.Y >= nodeYMax {
-						continue lineloop
-					}
-
-					edgeXMin := min(edge.a.X, edge.b.X)
-					edgeXMax := max(edge.a.X, edge.b.X)
-
-					if (edgeXMin <= nodeXMin && edgeXMax > nodeXMin) ||
-						(edgeXMin < nodeXMax && edgeXMax >= nodeXMax) {
-						continue rectloop
-					}
+				if edgeOverlapsRect(edge, rect) {
+					continue rectloop
 				}
 			}
 
